34816bfecc991a20b4dac99c65d540586289f9c7
tunamintasdev <142148175+tunamintasdev@users.noreply.github.com>
2025-01-23 23:14:53 +0300

    Update main.py

--- ml/main.py
+++ ml/main.py
@@ -11,12 +11,17 @@ player = Player()
 
 running = True
 
+clock = pygame.time.Clock()
+
+
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+            
+    player.update()
     screen.fill((0,0,0))
     map.render(screen)
     player.render(screen)
